2f96a31b8344a0cfd57918050c9ff507aeed0ad0
Aleksander Guryanov <caiiiycuk@gmail.com>
2013-01-03 15:56:26 +0700

    Yandex donation

--- portal/php/index.php
+++ portal/php/index.php
@@ -86,6 +86,8 @@ include 'helper.php';
       <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
       <img alt="" border="0" src="https://www.paypalobjects.com/ru_RU/i/scr/pixel.gif" width="1" height="1">
       </form>
+
+      <iframe allowtransparency="true" src="https://money.yandex.ru/embed/small.xml?uid=41001419739511&amp;button-text=06&amp;button-size=s&amp;button-color=black&amp;targets=dune+2+online+game&amp;default-sum=50" frameborder="0" height="31" scrolling="no" width="150px"></iframe>
     </div>
 
   </body>
